c6a2f6c6f531d913e6d4ee35cb1bd8a5a1dfc917
soerjantobj <soerjantobj@vcu.edu>
2024-01-08 17:26:18 -0500

    login, signup

--- Website/auth.py
+++ Website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__, template_folder="temps")
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-      return '<p>Login<p>'
+      return render_template("login.html")
 
-@auth.route('/register')
+@auth.route('/signup', methods=['GET', 'POST'])
 def register():
-      return '<p>Register<p>'
+      if request.method == 'POST':
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password1 = request.form.get('password1')
+            password2 = request.form.get('password2')
+
+            if len(password1) < 8:
+                  flash('Password must be at least 8 characters', category="error")
+            elif password1 != password2:
+                  flash('Passwords to not match', category="error")
+            elif len(username) < 4:
+                  flash('Username must be at least 4 characters', category="error")
+            elif '@' not in email:
+                  flash('Invalid Email', category="error")
+            elif len(email) < 3:
+                  flash('Email too short', category="error")
+            else:
+                  flash('Account created successfully', category="success")
+
+
 
 @auth.route('/logout')
 def logout():
-      return '<p>Logout<p>'
+      return render_template("logout.html")
--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint('views', __name__, template_folder="temps")
 
 @views.route('/')
 def home():
-	return "<h1>Movie Tracker</h1>"
+	return render_template("home.html")
